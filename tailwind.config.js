66a5524df3250f5176fe2272f5ad200a919ad4de
jmano@norwexic.com <jmano@norwexic.com>
2023-06-09 16:16:24 -0500

    added custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,32 @@ module.exports = {
     './public/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {fontSize:{jimmy:'5rem'}, 
+    colors:{
+      primary:'#71DBD4',
+      secondary:{
+        100:'#FFD319',
+        200:'#FF901F',
+      },
+      synth:{
+        orange:'#FF6C11',
+        red:'#FF3864',
+        aqua:'#2DE2E6',
+        darkblue:'#261447 ',
+        darkerblue:'#0D0221',
+        pink:'#F6019D',
+        fuschia:'#C724B1',
+      },},
+    fontFamily:{
+      pacman:['pacman','sans-serif'],
+      outrun:['outrun','sans-serif'],
+      brat:['brat','sans-serif'],
+      lazer84:['lazer84','sans-serif'],
+      indelible:['indelible','sans-serif'],
+      roadrage:['roadrage','sans-serif'],
+      streamster:['streamster','sans-serif'],
+      commando:['commando','sans-serif'],
+    }},
   },
   plugins: [],
 }
