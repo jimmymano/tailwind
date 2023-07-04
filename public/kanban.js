f70577aaad79df6fab5ef8a6bcb2c51e67cfa9ea
jmano@norwexic.com <jmano@norwexic.com>
2023-07-03 23:39:29 -0500

    created update DOM functionality

--- public/kanban.js
+++ public/kanban.js
@@ -7,16 +7,19 @@
 // .addbtn,addbtnsolid,addcontainer,additem 
 
 // addBtns is array of all buttons
-const addBtns = document.querySelectorAll('addbtn')
-const saveItemBtns = document.querySelectorAll('addbtnsolid')
-const addItemContainers = document.querySelectorAll('addcontainer')
-const addItems = document.querySelectorAll('additem')
+const addBtns = document.querySelectorAll('.addbtn')
+const saveItemBtns = document.querySelectorAll('.addbtnsolid')
+const addItemContainers = document.querySelectorAll('.addcontainer')
+const addItems = document.querySelectorAll('.additem')
 // Item Lists: itemLists = [backlogList,progressList,completeList,onHoldList]
-const itemLists = document.querySelectorAll('dragitemlist')
+const itemLists = document.querySelectorAll('.dragitemlist')
 const backlogList = document.getElementById('backloglist')
 const progressList = document.getElementById('progresslist')
 const completeList = document.getElementById('completelist')
-const onHoldList = document.getElementById('onholdlist')
+const onholdList = document.getElementById('onholdlist')
+
+// Items
+let updatedOnLoad = false;
 
 // Initialize Arrays: stores data for each of our lists
 // listArrays = list of all the other arrays, this is defined in updateSavedColumns()
@@ -61,11 +64,70 @@ function updateSavedColumns(){
     listArrays = [backlogListArray,progressListArray,completeListArray,onholdListArray];
     const arrayNames = ['backlog','progress','complete','onhold'];
     arrayNames.forEach((arrayName,index) =>{
-        localStorage.setItem(`${arrayName}Items`, JSON.stringify(listArrays[index]))
+        localStorage.setItem(`${arrayName}Item`, JSON.stringify(listArrays[index]))
     });
 }
-getSavedColumns();
+
+// Create DOM elements for each list item
+function createItemEl(columnEl,column,item,index){
+console.log('columnEl:',columnEl);
+console.log('column:',column);
+console.log('item:',item);
+console.log('index:',index);
+//List Item
+const listEl = document.createElement('li');
+listEl.classList.add('drag-item');
+listEl.textContent = item;
+// Append
+columnEl.appendChild(listEl);
+}
+
+// Update Columns in DOM - Reset HTML, Filter Array, Update localStorage
+
+function updateDOM(){
+// Check localStorage once: let globalVar boolean updatedOnLoad = falase;
+if(!updatedOnLoad){
+    getSavedColumns();
+}
+//Backlog Column 
+backlogList.textContent = ''
+backlogListArray.forEach((backlogItem,index)=>{
+    createItemEl(backlogList,0,backlogItem, index)
+});
+
+// Progress Column
+progressList.textContent = ''
+progressListArray.forEach((progressItem,index)=>{
+
+    createItemEl(progressList,0,progressItem, index)
+})
+
+// Complete Column
+completeList.textContent = ''
+completeListArray.forEach((completeItem,index)=>{
+    
+    createItemEl(completeList,0,completeItem, index)
+})
+
+// On Hold Column
+onholdList.textContent = ''
+onholdListArray.forEach((onholdItem,index)=>{
+    createItemEl(onholdList,0,onholdItem, index)
+})
+
+// Run getSavedcolumns only once, Update localStorage
+}
+
+
+
+
+
+
 updateSavedColumns();
+
+
+// On Load
+updateDOM();
 
 
 
